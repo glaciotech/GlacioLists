4bd51574b5dcafd72d6ea3427a3437050e3831c3
Peter Liddle <development.peter@liddle.network>
2023-02-20 17:17:36 -0600

    Initial Commit

---
+++ GlacioLists/GlacioListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlacioListsApp.swift
+//  GlacioLists
+//
+//  Created by Peter Liddle on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GlacioListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
