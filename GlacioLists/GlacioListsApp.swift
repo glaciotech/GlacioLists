824b3c6240fa9c4fb587dd4e161148e22ce6f397
Peter Liddle <development.peter@liddle.network>
2023-05-04 16:35:05 -0500

    - Add developerId and error

--- GlacioLists/GlacioListsApp.swift
+++ GlacioLists/GlacioListsApp.swift
@@ -17,10 +17,13 @@ struct GlacioListsApp: SwiftUI.App {
     let nodeManager: NodeManager
     let glacioCoordinator: GlacioRealmCoordinator
     
+    #error("Add developerId here. Can be obtained from Glacio portal - DELETE THIS LINE BEFORE RUNNING")
+    let developerId = ""
+    
     init() {
         
         do {
-            self.nodeManager = try NodeManager()
+            self.nodeManager = try NodeManager(developerId: developerId)
             self.realm = try Realm(configuration: Realm.Configuration(inMemoryIdentifier: "glaciolistdata", deleteRealmIfMigrationNeeded: true)) // We use ! here as if realm can't initialize our app won't work
 
             self.glacioCoordinator = try GlacioRealmCoordinator(realm: realm, nodeManager: nodeManager, objectsToMonitor: [ListItem.self])
